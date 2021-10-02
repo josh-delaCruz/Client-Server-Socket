268c74c03f20c9834e3f63984c5adc019ebc15fe
josh-delaCruz <75943649+josh-delaCruz@users.noreply.github.com>
2021-10-02 20:05:39 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Client cliente = new Client("ciao");
+        cliente.connetti();
+        cliente.comunica();
+    }
+}
